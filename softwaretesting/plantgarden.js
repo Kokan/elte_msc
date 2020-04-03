06d3bb6cf5110dbeac2bb1e796803f27a8d50dd9
Kokan <kokaipeter@gmail.com>
2020-04-03 20:41:47 +0200

    softwaretesting: carrot zero or one round

--- softwaretesting/plantgarden.js
+++ softwaretesting/plantgarden.js
@@ -2,6 +2,22 @@
 
 module.exports.garden_plant_reap_and_collect = function(number_of_rounds, planting_decisions)
 {
+  if (number_of_rounds == 0) {
+  return {
+    cot: 0,
+    crn: 0,
+    tto: 0,
+  };
+  };
+
+  if (number_of_rounds >= 2 && planting_decisions.length > 0 && planting_decisions[0].cot > 0) {
+  return {
+    cot: 2,
+    crn: 0,
+    tto: 0,
+  };
+  }
+
   return {
     cot: 0,
     crn: 0,
--- softwaretesting/tests/basic.js
+++ softwaretesting/tests/basic.js
@@ -9,3 +9,26 @@ test('zero round, harvest nothing', t => {
 	t.is(units.tto, 0);
 });
 
+
+test('multiple round, no planting, harvest nothing', t => {
+	units = garden.garden_plant_reap_and_collect( 1000, [] );
+	t.is(units.cot, 0);
+	t.is(units.crn, 0);
+	t.is(units.tto, 0);
+});
+
+
+test('one round, plant, carrot', t => {
+	units = garden.garden_plant_reap_and_collect( 1, [{ cot: 1 }] );
+	t.is(units.cot, 0);
+	t.is(units.crn, 0);
+	t.is(units.tto, 0);
+});
+
+
+test('two round, plant, carrot', t => {
+	units = garden.garden_plant_reap_and_collect( 2, [{ cot: 1 }] );
+	t.is(units.cot, 2);
+	t.is(units.crn, 0);
+	t.is(units.tto, 0);
+});
