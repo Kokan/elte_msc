ead31a424e48e741b5c8ac6be4e0aa58ca9330ed
Kokan <kokaipeter@gmail.com>
2020-04-03 20:30:37 +0200

    softwaretesting: harvest nothing in 0 step
    Signed-off-by: Kokan <kokaipeter@gmail.com>

---
+++ softwaretesting/plantgarden.js
@@ -0,0 +1,11 @@
+
+
+module.exports.garden_plant_reap_and_collect = function(number_of_rounds, planting_decisions)
+{
+  return {
+    cot: 0,
+    crn: 0,
+    tto: 0,
+  };
+}
+
--- softwaretesting/tests/basic.js
+++ softwaretesting/tests/basic.js
@@ -1,6 +1,11 @@
 const test = require('ava');
+const garden = require('../plantgarden')
 
-test('foo', t => {
-	t.pass();
+
+test('zero round, harvest nothing', t => {
+	units = garden.garden_plant_reap_and_collect( 0, [] );
+	t.is(units.cot, 0);
+	t.is(units.crn, 0);
+	t.is(units.tto, 0);
 });
 
